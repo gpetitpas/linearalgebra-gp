0fe3213932867b195b84cae74d2acfc1b53909a8
gpetitpas <gpetitpas@users.noreply.github.com>
2022-12-15 20:37:29 -0500

    Create vector file

---
+++ linearalgebra-gp/vector.py
@@ -0,0 +1,30 @@
+class Vector(object):
+    def __init__(self, coordinates):
+        try:
+            if not coordinates:
+                raise ValueError
+            self.coordinates = tuple(coordinates)
+            self.dimension = len(coordinates)
+
+        except ValueError:
+            raise ValueError('The coordinates must be nonempty')
+
+        except TypeError:
+            raise TypeError('The coordinates must be an iterable')
+
+
+    def __str__(self):
+        return 'Vector: {}'.format(self.coordinates)
+
+
+    def __eq__(self, v):
+        return self.coordinates == v.coordinates
+
+v1 = Vector([1,2,3])
+v2 = Vector([1,2,3])
+v3 = Vector([1,2,7])
+print(v3)
+print(v1 == v2)
+print(v1 == v3)
+
+# Next up: Lesson 2: Vectors, video #3
